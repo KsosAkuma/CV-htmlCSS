d12565e8159cf725115b456af66faf79c88a7aac
KsosAkuma <80045865+KsosAkuma@users.noreply.github.com>
2021-05-18 11:10:15 +0200

    test root of css

--- JS/customPage.js
+++ JS/customPage.js
@@ -8,25 +8,25 @@ let interrupteur = 0;
 pageAccueil = function(){
     switch (interrupteur){
         case 0 : 
-            quelStyle.href = "/CSS/style2.css";
+            quelStyle.href = "CSS/style2.css";
             btnStyle.textContent =" Mode : Grid";
             interrupteur = 1;
             break;
 
         case 1 : 
-            quelStyle.href = "/CSS/style1.1.css";
+            quelStyle.href = "CSS/style1.1.css";
             btnStyle.textContent =" Mode : Grid";
             interrupteur = 2;
             break;
 
         case 2 : 
-            quelStyle.href = "/CSS/style.css";
+            quelStyle.href = "CSS/style.css";
             btnStyle.textContent =" Mode : Flex";
             interrupteur = 0;
             break;
 
         default :
-            quelStyle.href = "/CSS/style.css";
+            quelStyle.href = "CSS/style.css";
             btnStyle.textContent =" Mode : Flex";
             interrupteur = 0;
             break;
